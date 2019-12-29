4cd3908d26f85a2362b6f4ba4c68b83acf0a0cd5
rzjrh <rzjrh@hotmail.com>
2019-12-29 14:44:43 -0600

    FirstTest

---
+++ src/main/java/com/example/demo/TestSpring1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpring1Application.class, args);
+	}
+
+}
